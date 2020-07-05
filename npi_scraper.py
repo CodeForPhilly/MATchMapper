e16aa8a6b55f499597552d186dc461e4c16699ff
Samuel <samueltan97@hotmail.com>
2020-07-04 23:55:04 -0400

    Updated scraper for other name and gender

--- npi_scraper.py
+++ npi_scraper.py
@@ -10,15 +10,20 @@ df.columns = df.iloc[0]
 df = df[1:]
 df = df[['firstname','lastname','who_id','npi']]
 npi_data = df['npi'].values.tolist()
+# npi_data = ['1205954138']
 npi_final_data = []
 for npi_id in npi_data:
     if type(npi_id) == str:
+        npi_dict = {}
         url = 'https://npiregistry.cms.hhs.gov/registry/provider-view/' + npi_id
 
         page = requests.get(url)
         doc = lh.fromstring(page.content)
         tr_elements = doc.xpath('//tr')
-        npi_dict = {}
+        el_elements = doc.xpath("//div[@class='row-fluid']")
+        gender_elements = doc.xpath("//span[@class='style1']")
+        gender_info = " ".join(gender_elements[0].text_content().split())
+        npi_dict['Gender'] = gender_info.split(' ')[1]
         # case_num is 0 if the person has no other name. case_num is 1 if the person has other name
         case_num = 1
         i=0
@@ -27,6 +32,19 @@ for npi_id in npi_data:
             name = t.text_content()
             if i == 3:
                 case_num = 0 if name == "Last Updated:" else 1
+        i = 0
+        for t in el_elements[0]:
+            i+=1
+            if i==2:
+                if case_num == 1:
+                    substring = " ".join(t.text_content().split())
+                    npi_dict['Name'] = substring.split('Other Name: ')[0]
+                    npi_dict['Other Name'] = substring.split('Other Name: ')[1].split(' Gender')[0]
+                else:
+                    substring = " ".join(t.text_content().split())
+                    npi_dict['Name'] = substring.split('Gender: ')[0]
+                    npi_dict['Other Name'] = ""           
+       
         i=0
         for t in tr_elements[case_num]:
             i+=1
