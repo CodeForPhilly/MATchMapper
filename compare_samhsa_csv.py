de88493e79365403685aa701944bd676ca480147
Samuel <samueltan97@hotmail.com>
2020-07-31 05:06:44 -0400

    Edited comparison samhsa script to generate list of removed providers, edited providers, and new providers

--- compare_samhsa_csv.py
+++ compare_samhsa_csv.py
@@ -1,40 +1,85 @@
 import pandas as pd
 import os
 
+class SAMHSA_Entry:
+
+    def __init__(self, name_prefix, first_name, last_name, address, address_complement, city, county, state, postal_code, telephone, fax):
+        self.name_prefix = str(name_prefix)
+        self.first_name = str(first_name)
+        self.last_name = str(last_name)
+        self.address = str(address)
+        self.address_complement = str(address_complement)
+        self.city = str(city)
+        self.county = str(county)
+        self.state = str(state)
+        self.postal_code = str(postal_code)
+        self.telephone = str(telephone)
+        self.fax = str(fax)
+
+     # OVERRIDE
+    def __str__(self):
+        return str("name_prefix: %s, first_name: %s, last_name: %s, address: %s, address_complement: %s, city: %s, county: %s, state: %s, postal_code: %s, telephone: %s, fax: %s" \
+        %(self.name_prefix, self.first_name, self.last_name, self.address, self.address_complement, self.city \
+        , self.county, self.state, self.postal_code, self.telephone, self.fax))
+
+    def __eq__(self, other):
+        if isinstance(other, SAMHSA_Entry):
+            return self.name_prefix == other.name_prefix and self.first_name == other.first_name and self.last_name == other.last_name
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash(self.name_prefix + ' ' + self.first_name + ' ' + self.last_name + ' ' + self.address + ' ' +  \
+        self.address_complement + ' ' + self.city + ' ' + self.county + ' ' + self.state + ' ' + self.postal_code \
+         + ' ' + self.telephone + ' ' + self.fax)
+
+
 path = os.getcwd()
 
-df_og = pd.read_csv(path + '\\BupePrescribersData - allnames_June2020.csv', index_col=False)
-df_1 = pd.read_csv(path + '\\Physician_Locator_06_23_2020_Phil-most-complete_515recs-after-removing-7-Philipsburg.csv', index_col=False)
-df_2 = pd.read_csv(path + '\\Physician_Locator_06_27_2020.csv', index_col=False)
+df_1 = pd.read_csv(os.path.join(path, 'samhsa_comparison_data', 'Physician_Locator_07_18_2020_513recs-Phila_still-plus-2.csv'), index_col=False)
+df_2 = pd.read_csv(os.path.join(path, 'samhsa_comparison_data', 'Physician_Locator_07_27_2020_513recs_Phila_still-plus-2.csv'), index_col=False)
 
-new_header = df_og.iloc[1] #grab the first row for the header
-df_og = df_og[2:] #take the data less the header row
-df_og.columns = new_header #set the header row as the df header
+df_1_set = set()
+df_2_set = set()
 
-df_og = df_og[['First Name','Last Name']]
-df_og = df_og.rename({'First Name': 'First', 'Last Name': 'Last'}, axis='columns')
-# df_intersection_og_1 = pd.merge(df_og, df_1, how='inner', on=['First', 'Last'])
-# df_intersection_og_1.dropna(inplace=True)
-# df_intersection_og_1.reset_index(inplace=True)
+for index, rows, in df_1.iterrows():
+    df_1_set.add(SAMHSA_Entry(rows['name_prefix'], rows['First'], rows['Last'], rows['Address'], rows['Address Complement'] \
+    , rows['City'], rows['County'], rows['State'], rows['Postal Code'], rows['Telephone'], rows['Fax']))
 
-df_1_duplicate = df_1[df_1.duplicated(subset=['First','Last'], keep=False)]
-df_2_duplicate = df_2[df_2.duplicated(subset=['First','Last'], keep=False)]
-print(df_2_duplicate)
+for index, rows, in df_2.iterrows():
+    df_2_set.add(SAMHSA_Entry(rows['name_prefix'], rows['First'], rows['Last'], rows['Address'], rows['Address Complement'] \
+    , rows['City'], rows['County'], rows['State'], rows['Postal Code'], rows['Telephone'], rows['Fax']))
 
-# df_diff_og_1 = df_1[(df_1.First.isin(df_og.First) == False) | (df_1.Last.isin(df_og.Last) == False)]
-# df_diff_og_1 = pd.concat([df_diff_og_1,df_1_duplicate],join='outer').drop_duplicates().reset_index(drop=True)
-# df_diff_og_1 = df_diff_og_1.merge(df_1_duplicate, on=['First', 'Last'], how='outer')
-# print(df_diff_og_1)
-# df_diff_og_1.to_csv(path + "\\06_23_minus_OG.csv", index=False)
-df_diff_1_og = df_og[(df_og.First.isin(df_1.First) == False) | (df_og.Last.isin(df_1.Last) == False)]
-# df_diff_1_og.to_csv(path + "\\OG_minus_06_23.csv", index=False)
+removed_or_edited = df_1_set - df_2_set
+edited_or_added = df_2_set - df_1_set
+
+removed = set()
+edited = set()
+added = set()
+
+for entry in removed_or_edited:
+    matched = False
+    for new_entry in edited_or_added:
+        if entry == new_entry:
+            edited.add(new_entry)
+            matched = True
+    if not matched:
+        removed.add(entry)
+
+for new_entry in edited_or_added:
+    matched = False
+    for entry in removed_or_edited:
+        if new_entry == entry:
+            matched = True
+            break
+    if not matched:
+        added.add(new_entry)
 
 
-#
-# df_diff_og_2 = df_2[(df_2.First.isin(df_og.First) == False) | (df_2.Last.isin(df_og.Last) == False)]
-# df_diff_og_2.to_csv(path + "\\06_27_minus_OG.csv", index=False)
-# df_diff_2_og = df_og[(df_og.First.isin(df_2.First) == False) | (df_og.Last.isin(df_2.Last) == False)]
-# df_diff_2_og.to_csv(path + "\\OG_minus_06_27.csv", index=False)
-# df_diff_og_1 = df_diff_og_1[df_diff_og_1.Last.isin(df_og.Last) == False]
-
-# print(df_diff_og_1)
+print([str(x) for x in removed])
+print()
+print([str(x) for x in edited])
+print()
+print([str(x) for x in added])
