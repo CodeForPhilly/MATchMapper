90678085dc7fe3ef0debc4a33f3d55937c20c597
Samuel <samueltan97@hotmail.com>
2021-02-24 14:40:40 -0500

    Removed redundant api/ in URL

--- database/bupehandler/urls.py
+++ database/bupehandler/urls.py
@@ -4,6 +4,6 @@ from . import views
 from . import api
 
 urlpatterns = [
-  path('api/<object_type>', api.object_list),
-  path('api/<object_type>/<int:oid>', api.single_object)
+  path('<object_type>', api.object_list),
+  path('<object_type>/<int:oid>', api.single_object)
 ]
