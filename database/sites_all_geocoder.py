a09a2de6d21671784c6e41edff6ab6c4f2ca5335
David <dbowden88@gmail.com>
2021-04-28 10:45:14 -0400

    Add files via upload
    added geocoded sties data and geocoder script

---
+++ database/sites_all_geocoder.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from geopy.geocoders import ArcGIS
+
+# setup geocoder
+geolocator = ArcGIS(user_agent="matchmapper")
+from geopy.extra.rate_limiter import RateLimiter
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=0.1)
+
+# load sites_all
+sites_all = pd.read_csv('database/sites_all.csv', encoding='cp1252')
+
+# collapse address to single vector
+addresses = sites_all['street_address'] + " " + "Philadelphia, PA" + sites_all['zip5'].astype(str)
+
+# geocode
+locations = addresses.apply(geocode)
+
+# extract lat and lon and fill into sites_all
+sites_all['latitude'] = locations.apply(lambda x: x.latitude)
+sites_all['longitude'] = locations.apply(lambda x: x.longitude)
+
+# write new csv
+sites_all.to_csv('database/sites_all_geocoded.csv')
